1aa646093b0f3fdeb6d052682c2adefa4139de8a
Ryan Dalke <rdalke@gmail.com>
2020-07-30 15:36:08 -0600

    added a color palette

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var artifactWidth  = 10;
-var artifactHeight = 10;
+var artifactWidth  = 25;
+var artifactHeight = 25;
 var pixelCount = artifactHeight*artifactWidth;
+
+var colorList = ['#fff','#f00', '#ff9d00', '#ff0', '#0f0', '#00f', '#a600ff', '#000'];
+var selectedColor = '#000';
 
 class Artifact extends React.Component {
 	constructor(props) {
@@ -18,7 +21,7 @@ class Artifact extends React.Component {
 
 	handleClick(i) {
 		const pixels = this.state.pixels.slice();
-		pixels[i] = ((this.state.pixels[i] === '#fff') ? '#000' : '#fff');
+		pixels[i] = selectedColor;
 		this.setState({
 			pixels: pixels,
 		})
@@ -69,9 +72,64 @@ function Pixel(props) {
 	);
 }
 
+// =============================================
+
+class Palette extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { palette: colorList,
+		};
+	}
+
+	renderInkwell(i) {
+		return (
+			<Inkwell 
+				index = {i}
+				color = {this.state.palette[i]}
+				onClick = {() => this.handleClick(i)}
+			/>
+		);
+	}
+
+	handleClick(i) {
+		selectedColor = this.state.palette[i];
+	}
+
+	render() {
+		const palette = [];
+		for (let i=0; i<this.state.palette.length; i++) {
+			palette.push(this.renderInkwell(i))
+		}
+		return (
+			<div className="palette">
+				{palette}
+			</div>
+		);
+	}
+}
+
+function Inkwell(props) {
+	return (
+		<button className="inkwell" index={props.index} onClick={props.onClick} style={{backgroundColor: props.color}} />
+	);
+}
+
+// ========================================
+
+class ArtifactEditor extends React.Component {
+	render() {
+		return(
+			<div className="artifactEditor">
+				<Artifact />
+				<Palette />
+			</div>
+		);
+	}
+}
+
 // ========================================
 
 ReactDOM.render(
-  <Artifact />,
+  <ArtifactEditor />,
   document.getElementById('root')
 );
